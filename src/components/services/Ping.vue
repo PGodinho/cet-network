aa13db56923bcccacbbd656e73378f00ca583faa
PGodinho <paulo.ufcd@gmail.com>
2023-08-13 17:41:15 +0100

    Neu

---
+++ src/components/services/Ping.vue
@@ -0,0 +1,83 @@
+<template>
+  <Generic :item="item">
+    <template #indicator>
+      <div v-if="status" class="status" :class="status">
+        {{ status }}
+      </div>
+    </template>
+  </Generic>
+</template>
+
+<script>
+import service from "@/mixins/service.js";
+import Generic from "./Generic.vue";
+
+export default {
+  name: "Ping",
+  mixins: [service],
+  props: {
+    item: Object,
+  },
+  components: {
+    Generic,
+  },
+  data: () => ({
+    status: null,
+  }),
+  created() {
+    this.fetchStatus();
+  },
+  methods: {
+    fetchStatus: async function () {
+      const method =
+        typeof this.item.method === "string"
+          ? this.item.method.toUpperCase()
+          : "HEAD";
+
+      if (!["GET", "HEAD", "OPTION"].includes(method)) {
+        console.error(`Ping: ${method} is not a supported HTTP method`);
+        return;
+      }
+
+      this.fetch("/", { method, cache: "no-cache" }, false)
+        .then(() => {
+          this.status = "online";
+        })
+        .catch(() => {
+          this.status = "offline";
+        });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.status {
+  font-size: 0.8rem;
+  color: var(--text-title);
+  white-space: nowrap;
+  margin-left: 0.25rem;
+
+  &.online:before {
+    background-color: #94e185;
+    border-color: #78d965;
+    box-shadow: 0 0 5px 1px #94e185;
+  }
+
+  &.offline:before {
+    background-color: #c9404d;
+    border-color: #c42c3b;
+    box-shadow: 0 0 5px 1px #c9404d;
+  }
+
+  &:before {
+    content: " ";
+    display: inline-block;
+    width: 7px;
+    height: 7px;
+    margin-right: 10px;
+    border: 1px solid #000;
+    border-radius: 7px;
+  }
+}
+</style>
